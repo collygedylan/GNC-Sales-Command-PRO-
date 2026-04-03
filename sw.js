9665b5e597b92f16612d00872cda8c695cedf308
collygedylan <dylancollyge@agmetricapp.com>
2026-04-03 14:46:14 -0500

    Allow JD to run script from app

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
    Optimized for: Instant Load, Offline Stability, Push Notifications, and staged shell updates.
 */
 
-const APP_SHELL_URL = './index.html?shellv=V2026.04.03.20';
-const CACHE_NAME = 'greenleaf-v4.2-rebuild-V2026.04.03.20';
+const APP_SHELL_URL = './index.html?shellv=V2026.04.03.21';
+const CACHE_NAME = 'greenleaf-v4.2-rebuild-V2026.04.03.21';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
