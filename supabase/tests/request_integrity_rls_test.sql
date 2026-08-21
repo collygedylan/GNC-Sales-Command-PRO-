4cf8b34d9c45a6d61eac153bd0adffc3b5d9549e
collygedylan <collygedylan@gmail.com>
2026-08-21 15:34:20 -0500

    Fix historical report pgTAP plan

--- supabase/tests/request_integrity_rls_test.sql
+++ supabase/tests/request_integrity_rls_test.sql
@@ -1,6 +1,6 @@
 begin;
 create extension if not exists pgtap with schema extensions;
-select plan(43);
+select plan(51);
 
 insert into auth.users (
   id, instance_id, aud, role, email, encrypted_password, email_confirmed_at,
